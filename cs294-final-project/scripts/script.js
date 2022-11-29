d833d2840cbae87d861d94502c73964ca4d8c1cf
Carl Flood <carl@carlflood.com>
2022-11-29 04:06:37 +0000

    Enabled Google Maps API + Place API to show local restaurants

---
+++ cs294-final-project/scripts/script.js
@@ -0,0 +1,77 @@
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+let map, infoWindow, service;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 41.8781, lng: -87.6298 },
+    zoom: 15,
+  });
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        map.setCenter(pos);
+        map.setZoom(15);
+        var marker = new google.maps.Marker({
+          position: pos,
+          map: map
+        });
+
+        var request = {
+          location: pos,
+          radius: '500',
+          type: ['restaurant']
+        };
+
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(
+  browserHasGeolocation,
+  infoWindow,
+  pos
+) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      new google.maps.Marker({
+        position: results[i].geometry.location,
+        map: map
+      });
+    }
+  }
+}
+
+window.initMap = initMap;
+export { };
